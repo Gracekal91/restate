ed0b063a60c8036f1a05da4ca1950550920ec41d
damien <damienmende30@gmail.com>
2022-01-14 19:39:45 +0200

    Fetching input from search form

--- src/components/search/rent/RentForm.js
+++ src/components/search/rent/RentForm.js
@@ -1,37 +1,61 @@
 import { Form, Row, Col, Button } from 'react-bootstrap'
 import { HiOutlineSearchCircle } from 'react-icons/hi'
 import { useState } from 'react'
-import millify from 'millify'
 
 
 const RentForm = ({ forRent }) => {
 
     let minPrices = [1000, 1500, 2000, 3000, 4000, 5000, 6000, 8000, 9000, 10000];
     let maxPrices = [1000, 1500, 2000, 3000, 4000, 5000, 6000, 8000, 9000, 10000, 20000, 30000, 4000, 50000];
+    let baths = forRent.map((item) => { return item.baths });
+    let numberOfBaths = [...new Set(baths)].sort();
 
-    const [minPrice, setMinPrice] = useState(1000);
-    const [maxPrice, setMaxPrice] = useState('any');
+    let rooms = forRent.map((item) => { return item.rooms });
+    let numberOfBeds = [...new Set(rooms)].sort();
 
+    let location1 = forRent.map((item) => {
+        return item.location[2].name
+    })
 
-    const update = (e) => {
-        e.preventDefault();
-        console.log(maxPrice);
+    console.log(location1)
+
+    const [minPrice, setMinPrice] = useState()
+    const [maxPrice, setMaxPrice] = useState()
+    const [bedRoom, setBedRoom] = useState()
+    const [bathRoom, setBathRoom] = useState()
+    //Handle multiple inputs on submits
+
+    const initialValues = Object.freeze({
+        minPrice: '',
+        maxPrice: '',
+        bedRoom: '',
+        bathRoom: ''
+    });
+
+    const [formData, setFormData] = useState(initialValues)
+
+    const handleForm = (e) => {
+        setFormData({
+            ...formData, [e.target.name]: e.target.value.trim()
+        })
     }
 
-    console.log(maxPrice);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
 
     return (
-        <form onSubmit={update}>
-            <Form.Group className="mb-3">
+        <form>
+            <div className="mb-3">
                 <Row className='mb-2'>
                     <Col>
-                        <Form.Select>
-                            <option placeholder='select price' selected disabled>Location</option>
+                        <select className='form-control'>
+                            <option selected disabled>Location</option>
                             {forRent.map((item) => {
                                 return <option placeholder='select price' >{item.price}</option>
                             })}
-
-                        </Form.Select>
+                        </select>
                     </Col>
                     <Col>
                         <Form.Select>
@@ -42,44 +66,43 @@ const RentForm = ({ forRent }) => {
                         </Form.Select>
                     </Col>
                     <Col>
-                        <Form.Select>
-                            <option placeholder='select price' selected disabled>Bedrooms</option>
-                            <option placeholder='select price' value=''> </option>
-                            <option placeholder='select price'>2,000</option>
-                            <option placeholder='select price' >3,000</option>
-                        </Form.Select>
+                        <select className='form-control' name='bedRoom' onChange={handleForm} value={bedRoom}>
+                            <option selected>Bedrooms</option>
+                            {numberOfBeds.map((item) => {
+                                return <option selected>{item}</option>
+                            })}
+
+                        </select>
                     </Col>
                 </Row>
                 <Row className='mb-2'>
                     <Col>
-                        <Form.Select>
-                            <option placeholder='select price' selected disabled>Bathrooms</option>
-                            <option placeholder='select price' >1,000</option>
-                            <option placeholder='select price'>2,000</option>
-                            <option placeholder='select price' >3,000</option>
-                        </Form.Select>
+                        <select className='form-control' name='bathRoom' onChange={handleForm} value={bathRoom}>
+                            <option selected>Bathroom</option>
+                            {numberOfBaths.map((item) => {
+                                return <option selected>{item}</option>
+                            })}
+                        </select>
                     </Col>
                     <Col>
-                        <Form.Select>
+                        <select className='form-control' name='minPrice' onChange={handleForm} value={minPrice}>
                             <option placeholder='select price' selected disabled>Min. Price</option>
                             {minPrices.map((item) => {
                                 return <option placeholder='select price' >R {item}</option>
                             })}
-                        </Form.Select>
+                        </select>
                     </Col>
                     <Col>
-                        <Form.Select>
-                            <option placeholder='select price' selected disabled>Max. Price</option>
+                        <select className='form-control' name='maxPrice' onChange={handleForm} value={maxPrice}>
+                            <option selected>Max price</option>
                             {maxPrices.map((item) => {
-                                return <option placeholder='select price'
-                                    value={maxPrice}
-                                    onSelect={(e) => setMaxPrice({ maxPrice: e.target.value })}>R {item}</option>
+                                return <option placeholder='select price' >R {item}</option>
                             })}
-                        </Form.Select>
+                        </select>
                     </Col>
                 </Row>
-                <Button stype='submit'><HiOutlineSearchCircle style={{ fontSize: '1.2rem', marginBottom: '2px', marginRight: '4px' }} />Search property for rent</Button>
-            </Form.Group>
+                <Button stype='submit' onClick={handleSubmit}><HiOutlineSearchCircle style={{ fontSize: '1.2rem', marginBottom: '2px', marginRight: '4px' }} />Search property for rent</Button>
+            </div>
         </form>
     )
 }
